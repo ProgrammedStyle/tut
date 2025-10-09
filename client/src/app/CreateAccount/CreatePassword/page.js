633a04f27cd28f646ef4372822fa01e9ab2f3f60
Mohammed Zaid <ProgrammedStyle@gmail.com>
2025-10-09 20:51:08 +0300

    third committt

--- client/src/app/CreateAccount/CreatePassword/page.js
+++ client/src/app/CreateAccount/CreatePassword/page.js
@@ -8,7 +8,7 @@ import InputText from "../components/InputText";
 import { Button } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import passwordSchema from "../../../../modules/userValidation_password.js";
+import passwordSchema from "../../../../../modules/userValidation_password.js";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
