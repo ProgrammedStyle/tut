cba178f7040a07d72164ac6c93a2ac7fe2438032
Mohammed Zaid <ProgrammedStyle@gmail.com>
2025-10-11 08:19:31 +0300

    sdd74ggsdess2fj4

--- server/controllers/user/email/verify/send.js
+++ server/controllers/user/email/verify/send.js
@@ -5,8 +5,8 @@ const send = async ( req, res ) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
+            user: `${process.env.SMTP_USER}`,
+            pass: `${process.env.SMTP_PASS}`
         }
     });
 
