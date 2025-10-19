17554c7f2a8fae0342993f1454ea802ec4da315e
Mohammed Zaid <ProgrammedStyle@gmail.com>
2025-10-19 15:06:47 +0300

    Fix infinite loading - prevent LoadingLink from showing loading on same-page navigation

--- client/src/app/components/LoadingLink/index.js
+++ client/src/app/components/LoadingLink/index.js
@@ -2,14 +2,18 @@
 
 import Link from 'next/link';
 import { useDispatch } from 'react-redux';
+import { usePathname } from 'next/navigation';
 import { showLoading } from '../../slices/loadingSlice';
 
 const LoadingLink = ({ href, children, style, className, ...props }) => {
     const dispatch = useDispatch();
+    const pathname = usePathname();
 
     const handleClick = (e) => {
-        // Show loading immediately on click
-        dispatch(showLoading());
+        // Only show loading if navigating to a different page
+        if (href !== pathname) {
+            dispatch(showLoading());
+        }
     };
 
     return (
